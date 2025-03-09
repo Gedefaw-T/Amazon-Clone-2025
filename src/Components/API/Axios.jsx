2573d0c31ed3a6b8a1c7581e80427314309784b3
Gedefaw-T <gedefawtigabum@gmail.com>
2025-03-09 13:30:27 +0300

    amazon frontend deployment

--- src/Components/API/Axios.jsx
+++ src/Components/API/Axios.jsx
@@ -1,8 +1,13 @@
 import axios from "axios"; 
 
 const axiosInstance = axios.create({
+    // firebaase local
     // baseURL: "http://127.0.0.1:5001/project-82dba/us-central1/api"
-    baseURL:"http://localhost:5000"
+    // local express API
+    // baseURL:"http://localhost:5000"
+
+    // deployed on render.com
+    baseURL: "https://amazonapi-deploy.onrender.com"
 })
 
 export {axiosInstance};
--- src/Pages/Utility/Firebase.jsx
+++ src/Pages/Utility/Firebase.jsx
@@ -3,13 +3,14 @@ import { getFirestore, doc, setDoc, collection } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAcM5wvKmiFRaT_H9QfZ9nGIqDCGSQrt_o",
-  authDomain: "project-82dba.firebaseapp.com",
-  projectId: "project-82dba",
-  storageBucket: "project-82dba.firebasestorage.app",
-  messagingSenderId: "355131584711",
-  appId: "1:355131584711:web:8c60c8c852b035eb818ee2",
-  measurementId: "G-45MTYXGTF0",
+  
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSEAGE_SENDER_ID,
+  appId: import.meta.env.VITE_APPID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
